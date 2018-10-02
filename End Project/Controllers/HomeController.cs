e8adf58e036b270cb7ef36e0fb8f0cfdf0969bf5
Vilmar Mouritsen <vilmarmouritsen@gmail.com>
2018-10-02 16:00:24 +0100

    her er broutt

--- End Project/Controllers/HomeController.cs
+++ End Project/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace End_Project.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        // GET: Home yes
         public ActionResult Index()
         {
             return View();
