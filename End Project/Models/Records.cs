82792dba0d467b01c0c3d0c83e2afce6c79e6fcb
Hans Kári Joensen <37090420+Derpaton@users.noreply.github.com>
2018-10-06 21:33:40 +0300

    Registration prototype

--- End Project/Models/Records.cs
+++ End Project/Models/Records.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.ComponentModel.DataAnnotations;
-
-namespace End_Project.Models {
+﻿namespace End_Project.Models
+{
     public class Records {
         public string Name { get; set; }
         public string Author { get; set; }
+        public string Genre { get; set; }
+        public string Year { get; set; }
     }
 }
---
+++ End Project/Models/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace End_Project.Models
+{
+    public class Users
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
